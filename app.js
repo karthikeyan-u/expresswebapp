741fa06c9f5c956ceadfddd19e7edd980e582fe1
ukarthikeyan <karthikeyan_u@hotmail.com>
2018-11-20 12:37:37 +0530

    get routes added

---
+++ app.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/',function(req,res){
+    res.render('main');
+});
+
+app.get('/members',function(req,res){
+    res.render('members');
+});
+
+app.get('/members/:profile_id',function(req,res){
+    let name = req.params.profile_id;
+    res.render('profile',{name: name});
+});
+
+app.listen(3000,function(){
+    console.log('Server Started');
+});
